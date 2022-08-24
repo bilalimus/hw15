2926d6018171d17f925667f3ad5559317c3b7b86
Maksat <bilalimus@gmail.com>
2022-08-25 01:23:17 +0600

    ExpenseDate.jsx translated to class

--- src/components/Expenses/ExpenseDate.jsx
+++ src/components/Expenses/ExpenseDate.jsx
@@ -1,28 +1,31 @@
-import './ExpenseDate.css'
+import { Component } from "react";
+import "./ExpenseDate.css";
 
-let thisYear = new Date()
-thisYear = thisYear.getFullYear()
+let thisYear = new Date();
+thisYear = thisYear.getFullYear();
 
-function ExpenseDate(props) {
-    let month = props.date.toLocaleString('en-US',{month: 'long'})
-    let day = props.date.toLocaleString('en-US',{day: '2-digit'})
-    let year = props.date.getFullYear()
-    let difference = thisYear - year
+class ExpenseDate extends Component {
+  render() {
+    let month = this.props.date.toLocaleString("en-US", { month: "long" });
+    let day = this.props.date.toLocaleString("en-US", { day: "2-digit" });
+    let year = this.props.date.getFullYear();
+    let difference = thisYear - year;
 
-    if (difference > 0){
-        year = `${difference} year(s) ago`
-    }else if (difference < 0){
-        year = 'future'
-    }else{
-        year = 'this year'
+    if (difference > 0) {
+      year = `${difference} year(s) ago`;
+    } else if (difference < 0) {
+      year = "future";
+    } else {
+      year = "this year";
     }
     return (
-        <div className='expense-date'>
-            <div className='expense-date__month'>{month}</div>
-            <div className='expense-date__day'>{day}</div>
-            <div className='expense-date__year'>{year}</div>
-        </div>
+      <div className="expense-date">
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__day">{day}</div>
+        <div className="expense-date__year">{year}</div>
+      </div>
     );
+  }
 }
 
 export default ExpenseDate;
